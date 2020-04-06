eb6f4400e0b8116c48ef2c4654feb861b7ff744c
Brett McGuire <bmcguir3@gmail.com>
2020-04-06 15:23:08 -0400

    getting started

---
+++ classes.py
@@ -0,0 +1,28 @@
+class workspace(object):
+
+	'''
+	The workspace class stores all of the information for an active processing session.
+	
+	Example
+	-------
+	
+		> ws = workspace()
+		
+	Notes
+	-----
+	
+	
+	
+	Attributes
+	----------
+	
+		
+		
+	'''
+	
+	def __init__(self):
+	
+		return
+		
+		
+#class 		
---
+++ ipython_quickstart.py
@@ -0,0 +1,1 @@
+from molsim import *
