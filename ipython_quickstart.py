d0977975e81af71c992a15b3fb74ed485f2c000a
Brett McGuire <bmcguir3@gmail.com>
2020-04-13 15:45:47 -0400

    added catalog reading and formatting

--- ipython_quickstart.py
+++ ipython_quickstart.py
@@ -2,3 +2,6 @@ import numpy as np
 from molsim import file_handling as fh
 from molsim import tests as tests
 from molsim.constants import ccm, cm, ckm, h, k, kcm
+from pkg_resources import resource_filename
+
+filepath = resource_filename(__name__,'tests/')
