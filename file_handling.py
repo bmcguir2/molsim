8ab20bb8cbd6863b06d49a7babe1fb7b77f7711c
Brett McGuire <bmcguir3@gmail.com>
2020-04-13 15:45:47 -0400

    added catalog reading and formatting

--- file_handling.py
+++ file_handling.py
@@ -1,6 +1,17 @@
 import numpy as np
 from molsim.constants import ccm, cm, ckm, h, k, kcm
 
+def _read_txt(filein):
+	'''Reads in any txt file and returns a line by line array'''
+	
+	return_arr = []
+	
+	with open(filein, 'r') as input:
+		for line in input:
+			return_arr.append(line)
+
+	return return_arr
+	
 def _read_freq_int(filein):
 	'''Reads in a two column frequency intensity file and returns the numpy arrays	'''
 	
@@ -16,3 +27,317 @@ def _read_freq_int(filein):
 	intensity = np.asarray(intensity)
 			
 	return frequency,intensity		
+	
+def _read_spcat(filein):
+	'''Reads an SPCAT catalog and returns spliced out numpy arrays'''
+	
+	#read in the catalog
+	raw_arr = _read_txt(filein)
+	
+	#set up some basic lists to populate
+	frequency = []
+	freq_err = []
+	logint = []
+	dof = []
+	elower = []
+	gup = []
+	tag = []
+	qnformat = []
+	qn1 = []
+	qn2 = []
+	qn3 = []
+	qn4 = []
+	qn5 = []
+	qn6 = []
+	qn7 = []
+	qn8 = []
+	qn9 = []
+	qn10 = []
+	qn11 = []
+	qn12 = []
+	
+	#split everything out	
+	for x in raw_arr:
+		frequency.append(x[:13].strip())
+		freq_err.append(x[13:21].strip())
+		logint.append(x[21:29].strip())
+		dof.append(x[29:31].strip())
+		elower.append(x[31:41].strip())
+		gup.append(x[41:44].strip())
+		tag.append(x[44:51].strip())
+		qnformat.append(x[51:55].strip())
+		qn1.append(x[55:57].strip())
+		qn2.append(x[57:59].strip())
+		qn3.append(x[59:61].strip())
+		qn4.append(x[61:63].strip())
+		qn5.append(x[63:65].strip())
+		qn6.append(x[65:67].strip())
+		qn7.append(x[67:69].strip())
+		qn8.append(x[69:71].strip())
+		qn9.append(x[71:73].strip())
+		qn10.append(x[73:75].strip())
+		qn11.append(x[75:77].strip())
+		qn12.append(x[77:].strip())
+		
+	#now go through and fix everything into the appropriate formats and make numpy arrays as needed
+	
+	#we start with the easy ones that don't have nonsense letters
+	frequency = np.array(frequency)
+	frequency = frequency.astype(np.float64)
+	freq_err = np.array(freq_err)
+	freq_err = freq_err.astype(np.float64)	
+	logint = np.array(logint)
+	logint = logint.astype(np.float64)	
+	dof = np.array(dof)
+	dof = dof.astype(np.int)
+	elower = np.array(elower)
+	elower = elower.astype(np.float64)
+	tag = np.array(tag)
+	tag = tag.astype(np.int)
+	qnformat = np.array(qnformat)
+	qnformat = qnformat.astype(np.int)	
+	
+	#now we use a sub-function to fix the letters and +/- that show up in gup, and the qns, and returns floats	
+	def _fix_spcat(x):
+		'''Fixes letters and +/- in something that's read in and returns floats'''
+		
+		#fix blanks - we just want them to be nice nones rather than empty strings
+		
+		if x == '':
+			return None
+			
+		#fix +/-: we'll just turn it into 1 or -1
+		
+		if x == '+':
+			return 1
+			
+		if x == '-':
+			return -1
+		
+		#and now the letters bullshit
+			
+		if 'A' in x:		
+			return 100 + int(x[1])
+		
+		if 'B' in x:		
+			return 110 + int(x[1])	
+		
+		if 'C' in x:		
+			return 120 + int(x[1])		
+
+		if 'D' in x:		
+			return 130 + int(x[1])
+		
+		if 'E' in x:		
+			return 140 + int(x[1])
+		
+		if 'F' in x:		
+			return 150 + int(x[1])
+		
+		if 'G' in x:		
+			return 160 + int(x[1])
+		
+		if 'H' in x:		
+			return 170 + int(x[1])				
+		
+		if 'I' in x:		
+			return 180 + int(x[1])	
+		
+		if 'J' in x:		
+			return 190 + int(x[1])
+		
+		if 'K' in x:		
+			return 200 + int(x[1])
+		
+		if 'L' in x:		
+			return 210 + int(x[1])
+		
+		if 'M' in x:		
+			return 220 + int(x[1])	
+		
+		if 'N' in x:		
+			return 230 + int(x[1])	
+		
+		if 'O' in x:		
+			return 240 + int(x[1])
+		
+		if 'P' in x:		
+			return 250 + int(x[1])
+		
+		if 'Q' in x:		
+			return 260 + int(x[1])	
+		
+		if 'R' in x:		
+			return 270 + int(x[1])
+		
+		if 'S' in x:		
+			return 280 + int(x[1])
+		
+		if 'T' in x:		
+			return 290 + int(x[1])	
+		
+		if 'U' in x:		
+			return 300 + int(x[1])	
+		
+		if 'V' in x:		
+			return 310 + int(x[1])
+		
+		if 'W' in x:		
+			return 320 + int(x[1])	
+		
+		if 'X' in x:		
+			return 330 + int(x[1])	
+		
+		if 'Y' in x:		
+			return 340 + int(x[1])	
+		
+		if 'Z' in x:		
+			return 350 + int(x[1])
+		
+		if 'a' in x:		
+			return 100 + int(x[1])
+		
+		if 'b' in x:		
+			return 110 + int(x[1])	
+		
+		if 'c' in x:		
+			return 120 + int(x[1])		
+
+		if 'd' in x:		
+			return 130 + int(x[1])
+		
+		if 'e' in x:		
+			return 140 + int(x[1])
+		
+		if 'f' in x:		
+			return 150 + int(x[1])
+		
+		if 'g' in x:		
+			return 160 + int(x[1])
+		
+		if 'h' in x:		
+			return 170 + int(x[1])				
+		
+		if 'i' in x:		
+			return 180 + int(x[1])	
+		
+		if 'j' in x:		
+			return 190 + int(x[1])
+		
+		if 'k' in x:		
+			return 200 + int(x[1])
+		
+		if 'l' in x:		
+			return 210 + int(x[1])
+		
+		if 'm' in x:		
+			return 220 + int(x[1])	
+		
+		if 'n' in x:		
+			return 230 + int(x[1])	
+		
+		if 'o' in x:		
+			return 240 + int(x[1])
+		
+		if 'p' in x:		
+			return 250 + int(x[1])
+		
+		if 'q' in x:		
+			return 260 + int(x[1])	
+		
+		if 'r' in x:		
+			return 270 + int(x[1])
+		
+		if 's' in x:		
+			return 280 + int(x[1])
+		
+		if 't' in x:		
+			return 290 + int(x[1])	
+		
+		if 'u' in x:		
+			return 300 + int(x[1])	
+		
+		if 'v' in x:		
+			return 310 + int(x[1])
+		
+		if 'w' in x:		
+			return 320 + int(x[1])	
+		
+		if 'x' in x:		
+			return 330 + int(x[1])	
+		
+		if 'y' in x:		
+			return 340 + int(x[1])	
+		
+		if 'z' in x:		
+			return 350 + int(x[1])
+			
+		return int(x)						
+	
+	#run the other arrays through the fixer, then convert them to what they need to be
+	
+	gup = [_fix_spcat(x) for x in gup]
+	gup = np.array(gup)
+	gup = gup.astype(int)
+	qn1 = [_fix_spcat(x) for x in qn1]
+	qn1 = np.array(qn1)
+	qn1 = qn1.astype(int) if all(y is not None for y in qn1) is True else qn1
+	qn2 = [_fix_spcat(x) for x in qn2]
+	qn2 = np.array(qn2)
+	qn2 = qn2.astype(int) if all(x is not None for x in qn2) is True else qn2			
+	qn3 = [_fix_spcat(x) for x in qn3]
+	qn3 = np.array(qn3)
+	qn3 = qn3.astype(int) if all(x is not None for x in qn3) is True else qn3	
+	qn4 = [_fix_spcat(x) for x in qn4]
+	qn4 = np.array(qn4)
+	qn4 = qn4.astype(int) if all(x is not None for x in qn4) is True else qn4	
+	qn5 = [_fix_spcat(x) for x in qn5]
+	qn5 = np.array(qn5)
+	qn5 = qn5.astype(int) if all(x is not None for x in qn5) is True else qn5	
+	qn6 = [_fix_spcat(x) for x in qn6]
+	qn6 = np.array(qn6)
+	qn6 = qn6.astype(int) if all(x is not None for x in qn6) is True else qn6	
+	qn7 = [_fix_spcat(x) for x in qn7]
+	qn7 = np.array(qn7)
+	qn7 = qn7.astype(int) if all(x is not None for x in qn7) is True else qn7	
+	qn8 = [_fix_spcat(x) for x in qn8]
+	qn8 = np.array(qn8)
+	qn8 = qn8.astype(int) if all(x is not None for x in qn8) is True else qn8	
+	qn9 = [_fix_spcat(x) for x in qn9]
+	qn9 = np.array(qn9)
+	qn9 = qn9.astype(int) if all(x is not None for x in qn9) is True else qn9	
+	qn10 = [_fix_spcat(x) for x in qn10]
+	qn10 = np.array(qn10)
+	qn10 = qn10.astype(int) if all(x is not None for x in qn10) is True else qn10	
+	qn11 = [_fix_spcat(x) for x in qn11]
+	qn11 = np.array(qn11)
+	qn11 = qn11.astype(int) if all(x is not None for x in qn11) is True else qn11	
+	qn12 = [_fix_spcat(x) for x in qn12]
+	qn12 = np.array(qn12)
+	qn12 = qn12.astype(int) if all(x is not None for x in qn12) is True else qn12	
+	
+	split_cat = {
+					'frequency'	: 	frequency,
+					'freq_err'	:	freq_err,
+					'logint'	:	logint,
+					'dof'		:	dof,
+					'elower'	:	elower,
+					'gup'		:	gup,
+					'tag'		:	tag,
+					'qnformat'	:	qnformat,
+					'qn1'		:	qn1,
+					'qn2'		:	qn2,
+					'qn3'		:	qn3,
+					'qn4'		:	qn4,
+					'qn5'		:	qn5,
+					'qn6'		:	qn6,
+					'qn7'		:	qn7,
+					'qn8'		:	qn8,
+					'qn9'		:	qn9,
+					'qn10'		:	qn10,
+					'qn11'		:	qn11,
+					'qn12'		:	qn12,
+				}
+	
+	return split_cat
+		
--- tests.py
+++ tests.py
@@ -4,14 +4,51 @@ from pkg_resources import resource_filename
 
 filepath = resource_filename(__name__,'tests/')
 
+def _test_molsim():
+	'''Meta function to run all validation and verification tests'''
+	
+	_val_molsim()
+	print('\n')
+	_ver_molsim()
+	
+	return
+
+def _val_molsim():
+	'''Meta function to run all validation test'''
+	
+	print('==========================')
+	print('Beginning Validation Tests')
+	print('==========================\n')
+	
+	_val_read_freq_int()
+	_val_read_txt()	
+	_val_read_spcat()
+	
+	return
+
+def _ver_molsim():
+	'''Meta function to run all verification test'''
+
+	print('============================')
+	print('Beginning Verification Tests')
+	print('============================\n')	
+	
+	_ver_read_freq_int()
+	_ver_read_txt()	
+	_ver_read_spcat()
+	
+	return	
+
 def _val_read_freq_int():
 
 	'''Validate the reading in of a frequency intensity delimited file'''
 
+	print('... validating file_handling._read_freq_int')
+
 	try:
 		frequency, intensity = fh._read_freq_int(filepath + 'file_handling/freq_int.txt')
 	except:
-		print('CRITICAL: file_handling._read_freq_int failed entirely to run.')
+		print('\tCRITICAL: file_handling._read_freq_int failed entirely to run.')
 		return
 		
 	warnings = 0	
@@ -19,60 +56,270 @@ def _val_read_freq_int():
 	#check that it has returned numpy arrays
 	
 	if isinstance(frequency,np.ndarray) is False:
-		print('WARNING: file_handling._read_freq_int returned "frequency" as {}.  Expected np.ndarray.' .format(type(frequency)))
+		print('\tWARNING: file_handling._read_freq_int returned "frequency" as {}.  Expected np.ndarray.' .format(type(frequency)))
 		warnings += 1
 		
 	if isinstance(intensity,np.ndarray) is False:
-		print('WARNING: file_handling._read_freq_int returned "intensity" as {}.  Expected np.ndarray.' .format(type(intensity)))
+		print('\tWARNING: file_handling._read_freq_int returned "intensity" as {}.  Expected np.ndarray.' .format(type(intensity)))
 		warnings += 1
 		
 	#check the length of the returned arrays
 	
 	if len(frequency) != 22:
-		print('WARNING: file_handling._read_freq_int returned "frequency" with a length of {}.  Expected 22.' .format(len(frequency)))
+		print('\tWARNING: file_handling._read_freq_int returned "frequency" with a length of {}.  Expected 22.' .format(len(frequency)))
 		warnings += 1			
 	
 	if len(intensity) != 22:
-		print('WARNING: file_handling._read_freq_int returned "intensity" with a length of {}.  Expected 22.' .format(len(intensity)))
+		print('\tWARNING: file_handling._read_freq_int returned "intensity" with a length of {}.  Expected 22.' .format(len(intensity)))
 		warnings += 1
 		
 	#check that the values are np.float64
 	
 	if any([isinstance(x,np.float64) for x in frequency]) is False:
-		print('WARNING: file_handling._read_freq_int returned "frequency" with values that were not np.float64.')
+		print('\tWARNING: file_handling._read_freq_int returned "frequency" with values that were not np.float64.')
 		warnings += 1
 		
 	if any([isinstance(x,np.float64) for x in intensity]) is False:
-		print('WARNING: file_handling._read_freq_int returned "intensity" with values that were not np.float64.')
+		print('\tWARNING: file_handling._read_freq_int returned "intensity" with values that were not np.float64.')
 		warnings += 1	
 		
-	print('Validation of file_handling._read_freq_int completed with {} warnings.' .format(warnings))						
+	print('\tValidation of file_handling._read_freq_int completed with {} warnings.\n' .format(warnings))						
 	
-	return
+	return warnings
 	
 def _ver_read_freq_int():
 
 	'''Verify the reading in of a frequency intensity delimited file'''
-
+	
+	print('... verifying file_handling._read_freq_int')
+	
 	try:
 		frequency, intensity = fh._read_freq_int(filepath + 'file_handling/freq_int.txt')
 	except:
-		print('CRITICAL: file_handling._read_freq_int failed entirely to run.')
+		print('\tCRITICAL: file_handling._read_freq_int failed entirely to run.')
 		return
-		
+
 	warnings = 0	
 
 	#check the total values
 	
 	if np.sum(frequency) != 29116838.174399998:
-		print('WARNING: file_handling._read_freq_int returned "frequency" with a total value of {}.  Expected 29116838.174399998.' .format(np.sum(frequency)))
+		print('\tWARNING: file_handling._read_freq_int returned "frequency" with a total value of {}.  Expected 29116838.174399998.' .format(np.sum(frequency)))
 		warnings += 1
 		
 	if np.sum(intensity) != -62.5124:
-		print('WARNING: file_handling._read_freq_int returned "intensity" with a total value of {}.  Expected 29116838.174399998.' .format(np.sum(intensity)))
+		print('\tWARNING: file_handling._read_freq_int returned "intensity" with a total value of {}.  Expected 29116838.174399998.' .format(np.sum(intensity)))
 		warnings += 1
 		
-	print('Verification of file_handling._read_freq_int completed with {} warnings.' .format(warnings))						
+	print('\tVerification of file_handling._read_freq_int completed with {} warnings.\n' .format(warnings))						
 	
-	return
+	return warnings
 	
+def _val_read_txt():
+
+	'''Validate the reading in of a generic text file into a line by line array'''
+	
+	print('... validating file_handling._read_txt')
+
+	try:
+		return_arr = fh._read_txt(filepath + 'file_handling/freq_int.txt')
+	except:
+		print('\tCRITICAL: file_handling._read_txt failed entirely to run.')
+		return
+		
+	warnings = 0	
+	
+	#check that it has returned a list
+	
+	if isinstance(return_arr,list) is False:
+		print('\tWARNING: file_handling._read_txt returned "return_arr" as {}.  Expected list.' .format(type(return_arr)))
+		warnings += 1
+		
+	#check the length of the returned list
+	
+	if len(return_arr) != 22:
+		print('\tWARNING: file_handling._read_txt returned "return_arr" with a length of {}.  Expected 22.' .format(len(return_arr)))
+		warnings += 1			
+		
+	#check that the values are strings
+	
+	if any([isinstance(x,str) for x in return_arr]) is False:
+		print('\tWARNING: file_handling._read_txt returned "return_arr" with values that were not strings.')
+		warnings += 1	
+		
+	print('\tValidation of file_handling._read_txt completed with {} warnings.\n' .format(warnings))						
+	
+	return warnings
+	
+def _ver_read_txt():
+
+	'''Verify the reading in of a generic text file into a line by line array'''
+	
+	print('... verifying file_handling._read_txt')
+	
+	try:
+		return_arr = fh._read_txt(filepath + 'file_handling/freq_int.txt')
+	except:
+		print('\tCRITICAL: file_handling._read_txt failed entirely to run.')
+		return	
+		
+	warnings = 0	
+
+	#check three values (start, middle, and end)
+
+	if return_arr[0].strip() != '115271.2018 -5.0105':
+		print('\tWARNING: file_handling._read_txt returned "return_arr" at index 0 of {}.  Expected 115271.2018 -5.0105' .format(return_arr[0].strip()))
+		warnings += 1
+
+	if return_arr[10].strip() != '1267014.4860 -2.3773':
+		print('\tWARNING: file_handling._read_txt returned "return_arr" at index 0 of {}.  Expected 1267014.4860 -2.3773' .format(return_arr[0].strip()))
+		warnings += 1
+		
+	if return_arr[-1].strip() != '2528172.0600 -2.9584':
+		print('\tWARNING: file_handling._read_txt returned "return_arr" at index 0 of {}.  Expected 2528172.0600 -2.9584' .format(return_arr[0].strip()))
+		warnings += 1		
+		
+	print('\tVerification of file_handling._read_txt completed with {} warnings.\n' .format(warnings))						
+	
+	return warnings
+	
+def _val_read_spcat():
+
+	'''Validate the reading in of an spcat formatted cat file'''
+	
+	print('... validating file_handling._read_spcat')
+
+	try:
+		split_cat = fh._read_spcat(filepath + 'file_handling/testcat.cat')
+	except:
+		print('\tCRITICAL: file_handling._read_spcat failed entirely to run.')
+		return
+		
+	warnings = 0
+	
+	frequency = split_cat['frequency']
+	freq_err = split_cat['freq_err']
+	logint = split_cat['logint']
+	dof = split_cat['dof']
+	elower = split_cat['elower']
+	gup = split_cat['gup']
+	tag = split_cat['tag']
+	qnformat = split_cat['qnformat']
+	qn1 = split_cat['qn1']
+	qn2 = split_cat['qn2']
+	qn3 = split_cat['qn3']
+	qn4 = split_cat['qn4']
+	qn5 = split_cat['qn5']
+	qn6 = split_cat['qn6']
+	qn7 = split_cat['qn7']
+	qn8 = split_cat['qn8']
+	qn9 = split_cat['qn9']
+	qn10 = split_cat['qn10']
+	qn11 = split_cat['qn11']
+	qn12 = split_cat['qn12']
+	
+	floats = ['frequency', 'freq_err', 'logint', 'elower']
+	ints = ['dof', 'gup', 'tag', 'qnformat', 'qn1', 'qn2', 'qn3', 'qn4', 'qn5', 'qn6', 'qn7', 'qn8', 'qn9', 'qn10', 'qn11', 'qn12']		
+	
+	#check that it has returned the appropriate types, with the appropriate lengths
+	
+	for x in split_cat:
+		if isinstance(split_cat[x],np.ndarray) is False:
+			print('\tWARNING: file_handling._read_spcat returned {} as {}.  Expected np.ndarray.' .format(x,type(split_cat[x])))
+			warnings += 1
+		if len(split_cat[x]) != 18:
+			print('\tWARNING: file_handling._read_spcat returned {} with a length of {}.  Expected 18.' .format(x,len(split_cat[x])))
+			warnings += 1			
+		
+	#check that the arrays that should be floats are floats
+	
+	for x in floats:
+		if all(y is None for y in split_cat[x]) is True:
+			continue
+		if any([isinstance(y,np.float64) for y in split_cat[x]]) is False:
+			print('\tWARNING: file_handling._read_spcat returned {} with values that were not np.float64 or None' .format(x))
+			warnings += 1	
+			
+	#check that the arrays that should be ints are ints
+	
+	for x in ints:
+		if all(y is None for y in split_cat[x]) is True:
+			continue
+		if any([isinstance(y,np.int64) for y in split_cat[x]]) is False:
+			print('\tWARNING: file_handling._read_spcat returned {} with values that were not np.int64 or None' .format(x))
+			warnings += 1		
+		
+	print('\tValidation of file_handling._read_spcat completed with {} warnings.\n' .format(warnings))						
+	
+	return warnings	
+	
+def _ver_read_spcat():
+
+	'''Verify the reading in of an spcat formatted cat file'''
+	
+	print('... verifying file_handling._read_spcat')
+
+	try:
+		split_cat = fh._read_spcat(filepath + 'file_handling/testcat.cat')
+	except:
+		print('\tCRITICAL: file_handling._read_spcat failed entirely to run.')
+		return
+		
+	warnings = 0
+	
+	frequency = split_cat['frequency']
+	freq_err = split_cat['freq_err']
+	logint = split_cat['logint']
+	dof = split_cat['dof']
+	elower = split_cat['elower']
+	gup = split_cat['gup']
+	tag = split_cat['tag']
+	qnformat = split_cat['qnformat']
+	qn1 = split_cat['qn1']
+	qn2 = split_cat['qn2']
+	qn3 = split_cat['qn3']
+	qn4 = split_cat['qn4']
+	qn5 = split_cat['qn5']
+	qn6 = split_cat['qn6']
+	qn7 = split_cat['qn7']
+	qn8 = split_cat['qn8']
+	qn9 = split_cat['qn9']
+	qn10 = split_cat['qn10']
+	qn11 = split_cat['qn11']
+	qn12 = split_cat['qn12']	
+	
+	#check some sums as a first pass
+	
+	sums = {
+				'frequency'	: 	23871.4071,
+				'freq_err'	:	4.252800000000001,
+				'logint'	:	-169.53010000000003,
+				'dof'		:	54,
+				'elower'	:	4053.3695,
+				'gup'		:	5832,
+				'tag'		:	1722,
+				'qnformat'	:	5472,
+				'qn1'		:	969,
+				'qn2'		:	207,
+				'qn3'		:	762,
+				'qn4'		:	955,
+				'qn5'		:	None,
+				'qn6'		:	None,
+				'qn7'		:	969,
+				'qn8'		:	207,
+				'qn9'		:	770,
+				'qn10'		:	969,
+				'qn11'		:	None,
+				'qn12'		:	None,	
+			}
+
+	for x in sums:
+		if sums[x] is None:
+			continue #this was covered in validation
+		if np.sum(split_cat[x]) != sums[x]:
+			print('\tWARNING: file_handling._read_spcat returned the sum of {} as {}.  Expected {}.' .format(x,np.sum(split_cat[x]),sums[x]))
+			warnings += 1
+		
+	print('\tVerification of file_handling._read_spcat completed with {} warnings.\n' .format(warnings))						
+	
+	return warnings	
