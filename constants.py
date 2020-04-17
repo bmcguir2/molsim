9c49667567bfd12de7d0720e76ce72ddd30852bd
Brett McGuire <bmcguir3@gmail.com>
2020-04-17 15:24:14 -0400

    adding catalog class

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 import scipy.constants
 
-ccm = scipy.constants.c * 100
-cm = scipy.constants.c
-ckm = scipy.constants.c * 0.001
-h = scipy.constants.h
-k = scipy.constants.k
-kcm = 0.69503476
+ccm = scipy.constants.c * 100 #speed of light in cm/s
+cm = scipy.constants.c #speed of light in m/s
+ckm = scipy.constants.c * 0.001 #speed of light in km/s
+h = scipy.constants.h #Planck's constant in Js
+k = scipy.constants.k #Boltzmann's constant in J/K
+kcm = scipy.constants.k * (scipy.constants.h**-1) * ((scipy.constants.c * 100)**-1) #Boltzmann's constants in cm-1/K
--- ipython_quickstart.py
+++ ipython_quickstart.py
@@ -3,5 +3,6 @@ from molsim import file_handling as fh
 from molsim import tests as tests
 from molsim.constants import ccm, cm, ckm, h, k, kcm
 from pkg_resources import resource_filename
+from molsim.classes import workspace, catalog
 
 filepath = resource_filename(__name__,'tests/')
