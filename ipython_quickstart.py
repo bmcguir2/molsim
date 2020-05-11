eca2462a6b22727d1d5c4a7f15e28509b06d7b0b
Brett McGuire <bmcguir3@gmail.com>
2020-05-11 13:18:22 -0400

    speedups for loading catalogs

--- ipython_quickstart.py
+++ ipython_quickstart.py
@@ -4,7 +4,7 @@ sys.path.insert(0, '/Users/Brett/Dropbox/Programs')
 import numpy as np
 from molsim import file_handling as fh
 from molsim.file_handling import load_mol
-from molsim import tests as tests
+#from molsim import tests as tests
 from molsim.constants import ccm, cm, ckm, h, k, kcm
 from pkg_resources import resource_filename
 from molsim.classes import Workspace, Catalog, Transition, Level, Molecule, PartitionFunction
