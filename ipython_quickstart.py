7c17af24b62d3f2bc8bfa4d5ed18681fd49da64a
Brett McGuire <bmcguir3@gmail.com>
2020-06-12 14:32:33 -0400

    added find_limits function

--- ipython_quickstart.py
+++ ipython_quickstart.py
@@ -8,7 +8,7 @@ from molsim.constants import ccm, cm, ckm, h, k, kcm
 from pkg_resources import resource_filename
 from molsim.classes import Workspace, Catalog, Transition, Level, Molecule, PartitionFunction, Spectrum, Simulation, Continuum, Source, Observatory, Observation
 from molsim.stats import get_rms
-from molsim.utils import _trim_arr, find_nearest, _make_gauss, _apply_vlsr
+from molsim.utils import _trim_arr, find_nearest, _make_gauss, _apply_vlsr, find_limits
 import matplotlib.pyplot as plt
 import matplotlib
 import math
