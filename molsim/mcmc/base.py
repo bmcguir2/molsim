f134c5f509734a96671ffc7692e1eb22e4e53283
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-09-09 09:58:02 -0400

    Implemented a bunch of methods and docstrings
    Migrating to use the MolSim simulation method instead of the redundant
    code; seems like it's easier to deal with partition functions that way.

--- molsim/mcmc/base.py
+++ molsim/mcmc/base.py
@@ -10,9 +10,18 @@ import emcee
 
 from molsim.mcmc import compute
 
+"""
+These namedtuples are used to compactly represent parameters
+for a likelihood function. Currently there are two implemented
+based on what was used for the GOTHAM data: uniform and gaussian.
+"""
 
-UniformParameter = namedtuple("UniformParameter", "min max")
-GaussianParameter = namedtuple("GaussianParameter", "mu var min max")
+UniformParameter = namedtuple(
+    "UniformParameter", "min max", defaults=(-np.inf, np.inf)
+)
+GaussianParameter = namedtuple(
+    "GaussianParameter", "mu var min max", defaults=(0.0, 1.0, 0.0, np.inf)
+)
 
 
 def str2class(classname: str):
@@ -27,7 +36,7 @@ class AbstractDistribution(ABC):
     def __init__(self, name: str):
         super().__init__()
         self._name = name
-        self._param = UniformParameter(-1.0, 1.0)
+        self._param = UniformParameter()
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__} for {self.name()}, limits {self.limits()}"
@@ -107,15 +116,15 @@ class UniformLikelihood(AbstractDistribution):
 class GaussianLikelihood(AbstractDistribution):
     def __init__(self, name: str, param: GaussianParameter):
         """
-        [summary]
+        A likelihood class representing a normal distribution, centered
 
         Parameters
         ----------
         name : str
             [description]
-        limits : Tuple[float]
-            Two-tuple containing the Gaussian center (mu) and variance (var)
-            that defines the normal distribution N~(mu, var)
+        param : GaussianParameter
+            Requires a `GaussianParameter` namedtuple, with center mu
+            and variance var, and 
         """
         super().__init__(name)
         self._param = param
@@ -141,6 +150,21 @@ class GaussianLikelihood(AbstractDistribution):
 
     @classmethod
     def from_npy_chain(cls, name: str, chain: np.ndarray, min=0.0, max=np.inf):
+        """
+        Uses a chain of parameter samples, as a NumPy 1D array, to compute the mean
+        and variance as parameters for a new `GaussianLikelihood` instance.
+
+        Parameters
+        ----------
+        name : str
+            [description]
+        chain : np.ndarray
+            [description]
+        min, max : float, optional
+            Min/max values that this parameter can take, by default 0.0 and np.inf.
+            If the sampled value exceeds this, we return negative infinity as the
+            likelihood.
+        """
         percentiles = np.percentile(chain, [16.0, 50.0, 84.0])
         var = np.mean([percentiles[0], percentiles[-1]])
         mu = percentiles[1]
@@ -159,9 +183,6 @@ class GaussianLikelihood(AbstractDistribution):
 
 
 class AbstractModel(ABC):
-    def __init__(self):
-        super().__init__()
-
     @abstractmethod
     def simulate_spectrum(self, parameters: np.ndarray) -> np.ndarray:
         raise NotImplementedError
@@ -173,163 +194,6 @@ class AbstractModel(ABC):
     @abstractmethod
     def compute_log_likelihood(self, parameters: np.ndarray) -> float:
         raise NotImplementedError
-
-
-@dataclass
-class SingleComponent(AbstractModel):
-    source_size: AbstractDistribution
-    vlsr: AbstractDistribution
-    Ncol: AbstractDistribution
-    Tex: AbstractDistribution
-    dV: AbstractDistribution
-    calc_Q: Union[Callable, str]
-    spectrum: Type["DataChunk"]
-    catalog: Type["Catalog"]
-    dish_size: float = 100.0
-    background_temperature: float = 2.725
-
-    def __post_init__(self):
-        self._distributions = [
-            self.source_size,
-            self.vlsr,
-            self.Ncol,
-            self.Tex,
-            self.dV,
-        ]
-
-    def __len__(self) -> int:
-        return len(self._distributions)
-
-    def _get_components(self):
-        return self._distributions
-
-    def simulate_spectrum(self, parameters: np.ndarray) -> np.ndarray:
-        _, intensity = compute.build_synthetic_spectrum(
-            *parameters,
-            calc_Q=self.calc_Q,
-            spectrum=self.spectrum,
-            catalog=self.catalog,
-            dish_size=self.dish_size,
-            background_temperature=self.background_temperature,
-        )
-        return intensity
-
-    def compute_prior_likelihood(self, parameters: np.ndarray) -> float:
-        lnlikelihood = sum(
-            [
-                dist.ln_likelihood(value)
-                for dist, value in zip(self._distributions, parameters)
-            ]
-        )
-        return lnlikelihood
-
-    def compute_log_likelihood(self, parameters: np.ndarray) -> float:
-        simulation = self.simulate_spectrum(parameters)
-        inv_sigmasq = 1.0 / (self.spectrum.noise ** 2.0)
-        tot_lnlike = np.sum(
-            (self.spectrum.intensity - simulation) ** 2 * inv_sigmasq
-            - np.log(inv_sigmasq)
-        )
-        return -0.5 * tot_lnlike
-
-
-class MultiComponent(AbstractModel):
-    """
-    Implementation of a multi component model. This type of model extends
-    the parameters expected with parameters for each component like so:
-    
-    [source_size1, source_size2, vlsr1, vlsr2,...Tex, dV]
-    
-    So that there is an arbitrary number of components, providing each
-    component has a source size, radial velocity, and column density.
-    """
-
-    def __init__(
-        self,
-        source_sizes: List[AbstractDistribution],
-        vlsrs: List[AbstractDistribution],
-        Ncols: List[AbstractDistribution],
-        Tex: AbstractDistribution,
-        dV: AbstractDistribution,
-        calc_Q: Union[Callable, str],
-        spectrum: Type["DataChunk"],
-        catalog: Type["Catalog"],
-        dish_size: float = 100.0,
-        background_temperature: float = 2.725,
-    ):
-        super().__init__()
-        assert len(source_sizes) == len(vlsrs) == len(Ncols)
-        self.components = list()
-        for ss, vlsr, Ncol in zip(source_sizes, vlsrs, Ncols):
-            self.components.append(
-                SingleComponent(
-                    ss,
-                    vlsr,
-                    Ncol,
-                    Tex,
-                    dV,
-                    calc_Q,
-                    spectrum,
-                    catalog,
-                    dish_size,
-                    background_temperature,
-                )
-            )
-
-    def __len__(self) -> int:
-        return len(self.components) * 3 + 2
-
-    def _get_components(self):
-        params = list()
-        for index, component in enumerate(self.components):
-            if index != len(self.components):
-                params.extend(component._distributions[:3])
-            else:
-                params.extend(component._distributions)
-        return params
-
-    def _get_component_parameters(
-        self, parameters: np.ndarray, component: int
-    ) -> np.ndarray:
-        subparams = parameters[component:-2:4]
-        # tack on the excitation temperature and linewidth, which are global
-        subparams = np.append(subparams, [parameters[-2], parameters[-1]])
-        return subparams
-
-    def simulate_spectrum(self, parameters: np.ndarray) -> np.ndarray:
-        Tex, dV = parameters[-2], parameters[-1]
-        intensity = np.zeros_like(self.spectrum.frequency)
-        for index, component in enumerate(self.components):
-            # take every third element, corresponding to a parameter for
-            # a particular component. Skip the last two, which are
-            subparams = self._get_component_parameters(parameters, index)
-            intensity += component.simulate_spectrum(
-                subparams,
-                calc_Q=self.calc_Q,
-                spectrum=self.spectrum,
-                catalog=self.catalog,
-                dish_size=self.dish_size,
-                background_temperature=self.background_temperature,
-            )
-        return intensity
-
-    def compute_prior_likelihood(self, parameters: np.ndarray) -> float:
-        for index, component in enumerate(self.components):
-            subparams = self._get_component_parameters(parameters, index)
-            if index == 0:
-                lnlikelihood = component.compute_prior_likelihood(subparams)
-            else:
-                lnlikelihood += component.compute_prior_likelihood(subparams)
-        return lnlikelihood
-
-    def compute_log_likelihood(self, parameters: np.ndarray) -> float:
-        simulation = self.simulate_spectrum(parameters)
-        inv_sigmasq = 1.0 / (self.spectrum.noise ** 2.0)
-        tot_lnlike = np.sum(
-            (self.spectrum.intensity - simulation) ** 2 * inv_sigmasq
-            - np.log(inv_sigmasq)
-        )
-        return -0.5 * tot_lnlike
 
 
 class EmceeHelper(object):
@@ -366,6 +230,24 @@ class EmceeHelper(object):
 
 
 def compute_model_likelihoods(parameters: np.ndarray, model: AbstractModel) -> float:
+    """
+    Wrapper function used in `emcee` sampling calls. This implements the
+    abstraction needed to bridge the two aspects of our code: parameters
+    that are sampled, and a "model" that dictates the prior likelihoods
+    and how a spectrum is simulated.
+
+    Parameters
+    ----------
+    parameters : np.ndarray
+        [description]
+    model : AbstractModel
+        [description]
+
+    Returns
+    -------
+    float
+        [description]
+    """
     prior = model.compute_prior_likelihood(parameters)
     if np.isfinite(prior):
         return prior + model.compute_loglikelihood(parameters)
