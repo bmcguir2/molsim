b61df1e5bf9fa20281936392639aff1527fed2f7
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-09-22 12:57:59 -0400

    Possibly solving the VLSR issue
    Instead of a nominal VLSR, the offsets to min/max per window are now
    calculated using the input parameters.
    
    This means we can depreciate the keyword, but for now I'd like to test
    that it makes sense first.

--- molsim/mcmc/__init__.py
+++ molsim/mcmc/__init__.py
@@ -3,3 +3,4 @@ from . import base, compute, models, preprocess
 
 from molsim.mcmc.base import EmceeHelper
 from molsim.mcmc.models import TMC1FourComponent, MultiComponent, SingleComponent
+from molsim.mcmc.preprocess import DataChunk
