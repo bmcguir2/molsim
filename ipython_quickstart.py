7d9d7bc4c5982ed4b59ad5be0ea2f25d4a6ac215
Brett McGuire <bmcguir3@gmail.com>
2020-10-05 11:30:52 -0400

    adds json helper function

--- ipython_quickstart.py
+++ ipython_quickstart.py
@@ -8,7 +8,7 @@ from molsim.constants import ccm, cm, ckm, h, k, kcm
 #from pkg_resources import resource_filename
 from molsim.classes import Workspace, Catalog, Transition, Level, Molecule, PartitionFunction, Spectrum, Simulation, Continuum, Source, Observatory, Observation, Iplot, Trace
 from molsim.stats import get_rms
-from molsim.utils import _trim_arr, find_nearest, _make_gauss, _apply_vlsr, find_limits, find_peaks, _get_res, _find_nans, _find_limit_idx, generate_spcat_qrots
+from molsim.utils import _trim_arr, find_nearest, _make_gauss, _apply_vlsr, find_limits, find_peaks, _get_res, _find_nans, _find_limit_idx, generate_spcat_qrots, process_mcmc_json
 from molsim.functions import sum_spectra, velocity_stack, matched_filter, convert_spcat, resample_obs
 from molsim.plotting import plot_mf, plot_stack, plot_sim
 from molsim.analysis import set_upper_limit
@@ -17,5 +17,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import math
 from scipy import signal
+import json
+
 
 #filepath = resource_filename(__name__,'tests/')
