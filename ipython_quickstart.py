f7b230141485f6396ad42bcf0ade60785690ffd6
Brett McGuire <bmcguir2@gmail.com>
2020-10-06 10:44:56 -0400

    allows Jy/beam fitting in do_lsf

--- ipython_quickstart.py
+++ ipython_quickstart.py
@@ -5,7 +5,7 @@ import numpy as np
 from molsim.file_handling import load_mol, load_obs
 from molsim.file_io import _write_xy
 from molsim.constants import ccm, cm, ckm, h, k, kcm
-#from pkg_resources import resource_filename
+from molsim.fitting import do_lsf
 from molsim.classes import Workspace, Catalog, Transition, Level, Molecule, PartitionFunction, Spectrum, Simulation, Continuum, Source, Observatory, Observation, Iplot, Trace
 from molsim.stats import get_rms
 from molsim.utils import _trim_arr, find_nearest, _make_gauss, _apply_vlsr, find_limits, find_peaks, _get_res, _find_nans, _find_limit_idx, generate_spcat_qrots, process_mcmc_json
