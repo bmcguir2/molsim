5df5532a59e5fcc47a7ee16fa899017282dc356a
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-02-11 12:50:57 -0500

    Updated deterministic model sample tests

--- molsim/mcmc/tests/test_mcmc.py
+++ molsim/mcmc/tests/test_mcmc.py
@@ -25,6 +25,8 @@ def test_likelihoods():
 
 
 def test_single_component():
+    seed = np.random.seed(42)
+
     source_size = UniformLikelihood.from_values("ss", 0., 400.)
     ncol = UniformLikelihood.from_values("ncol", 0., 1e16)
     vlsr = UniformLikelihood.from_values("vlsr", 0., 10.)
@@ -37,13 +39,18 @@ def test_single_component():
         ncol,
         Tex,
         dV,
-        None,
+        None,   # no molecule required
+        None    # no observation required
         )
-    initial = model.initialize_values()
-    assert initial == [200., 5., 5e15, 5.8, 0.1]
+    # check the intialization routine is still working
+    initial = model.sample_prior()
+    static = [1.49816048e+02, 9.50714306e+00, 7.31993942e+15, 5.24405994e+00, 1.31890218e-01]
+    assert np.allclose(initial, static)
 
 
 def test_multi_component():
+    seed = np.random.seed(42)
+
     source_sizes = [UniformLikelihood.from_values("ss", 0., 400.) for _ in range(4)]
     vlsrs = [UniformLikelihood.from_values("vlsr", 0., 10.) for _ in range(4)]
     ncols = [UniformLikelihood.from_values("ncol", 0., 1e16) for _ in range(4)]
@@ -57,12 +64,18 @@ def test_multi_component():
         Tex,
         dV,
         None,
+        None,
         )
 
     # should be 14 parameters total; 3 * 4 components + 2
     assert len(model) == 14
 
     model._get_components()
+
+    # check the intialization routine is still working deterministically when asked to be
+    initial = model.sample_prior()
+    static = [149.81604753894499, 62.39780813448106, 8.233797718320979, 73.36180394137352, 9.50714306409916, 0.5808361216819946, 9.699098521619943, 3.0424224295953772, 7319939418114051.0, 8661761457749352.0, 8324426408004217.0, 5247564316322378.0, 4.937541083743484, 0.043771247075902735]
+    assert np.allclose(initial, static)
 
     # make sure the combined likelihood looks reasonable
     parameters = [100., 96, 20, 45, 5., 5.6, 6.44, 4.3, 1e10, 1e11, 1e9, 1e11, 5.87, 0.09216]
