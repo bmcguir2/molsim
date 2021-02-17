b09a1ba40bcb18bff35cf65d6c8275193cd43408
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-02-17 11:17:25 -0500

    Seemingly working composite model?
    Code seems to be nominally working, until I find a bug somewhere. This
    is a very strange feeling.

--- molsim/mcmc/models.py
+++ molsim/mcmc/models.py
@@ -1,4 +1,5 @@
 from typing import Type, Dict, Union, Callable, List, Tuple, Iterable
+from functools import lru_cache
 from dataclasses import dataclass
 
 import numpy as np
@@ -633,3 +634,213 @@ class CospatialTMC1(TMC1FourComponent):
             return 0.
         else:
             return -np.inf
+
+
+class CompositeModel(AbstractModel):
+    """
+    Implements an abstract composite model. Using this by
+    itself will not work as parameter sharing is not implemented
+    correctly, but basically lays out the recipe for implementing
+    real composite models.
+    """
+    def __init__(self, param_indices: List[List[int]], *models, **kwargs) -> None:
+        """
+        Parameters
+        ----------
+        param_indices : List[List[int]]
+            Nested list of integers corresponding to the
+            parameter indices of each model. The top list
+            should be the same length as the number of models,
+            and each sublist should have the same number of
+            indices as expected by each model.
+        """
+        assert len(param_indices) == len(list(models))
+        super().__init__()
+        self.models = list(models)
+        self.param_indices = param_indices
+
+    def __len__(self) -> int:
+        # do a messy flattened list comprehension and get the largest
+        # index out, plus one for the number of parameters
+        return max([index for sublist in self.param_indices for index in sublist]) + 1
+
+    def __repr__(self):
+        output_string = f"Composite model\nNumber of parameters: {len(self)}\n"
+        for model in self.models:
+            output_string += f"Model: {model}\n"
+        return output_string
+
+    def model_parameter(self, parameters: np.ndarray, index: int) -> np.ndarray:
+        """
+        Get the model parameters associated with a particular submodel.
+        All this function does is cross reference the model index with
+        the `param_indices` attribute, and returns the parameter values
+        associated with that particular model.
+
+        Parameters
+        ----------
+        parameters : np.ndarray
+            NumPy 1D array containing the full parameter values
+        index : int
+            Model index number
+
+        Returns
+        -------
+        np.ndarray
+            NumPy 1D array containing the parameters
+            for the specified submodel
+        """
+        return parameters[self.param_indices[index]]
+
+    @lru_cache(maxsize=2, typed=True)
+    @property
+    def frequency(self) -> np.ndarray:
+        """
+        Returns a common frequency grid for all the models considered. This allows
+        for a combined simulation to be performed.
+        
+        This property should also be cached, as to prevent constant recalculation
+        of the frequency grid.
+
+        Returns
+        -------
+        np.ndarray
+            NumPy 1D array of frequencies
+        """
+        frequency = [model.observation.spectrum.frequency for model in self.models]
+        # get unique values of frequency and then sort
+        frequency = np.unique(np.concatenate(frequency))
+        frequency.sort()
+        return frequency
+
+    @lru_cache(maxsize=2, typed=True)
+    def simulate_spectrum(self, parameters: np.ndarray) -> np.ndarray:
+        """
+        Simulate the composite spectrum for multiple models. This uses
+        the `param_indices` attribute to properly allocate parameters
+        to each model.
+
+        Parameters
+        ----------
+        parameters : np.ndarray
+            NumPy 1D array containing all of the model parameters.
+
+        Returns
+        -------
+        np.ndarray
+            NumPy 1D array containing the simulated intensities combined
+            from each model.
+        """
+        spectrum = np.zeros_like(self.frequency)
+        for index, model in enumerate(self.models):
+            # retrieve the correct parameters
+            subparams = self.model_parameter(parameters, index)
+            temp_spectrum = model.simulate_spectrum(subparams)
+            # add the interpolated spectrum
+            spectrum += np.interp(self.frequency, model.observation.spectrum.frequency, temp_spectrum, left=0., right=0.)
+        return spectrum
+
+    def compute_prior_likelihood(self, parameters: np.ndarray) -> float:
+        """
+        Compute the composite prior likelihood, combined from all of
+        the submodels. Note that this function applies a top-level
+        prior constraint implemented in `CompositeModel`, in addition
+        to the submodel constraints.
+
+        Parameters
+        ----------
+        parameters : np.ndarray
+            NumPy 1D array containing the full sampled parameters
+
+        Returns
+        -------
+        float
+            Prior log likelihood summed over all submodels
+        """
+        likelihood = self.prior_constraint(parameters)
+        for index, model in enumerate(self.models):
+            subparams = self.model_parameter(parameters, index)
+            likelihood += model.compute_prior_likelihood(subparams)
+        return likelihood
+
+    def compute_log_likelihood(self, parameters: np.ndarray) -> float:
+        """
+        Compute the composite log likelihood, combined from all of
+        the models.
+
+        Parameters
+        ----------
+        parameters : np.ndarray
+            NumPy 1D array containing the full sampled parameters
+
+        Returns
+        -------
+        float
+            Log likelihood summed over all submodels
+        """
+        likelihood = sum([model.compute_log_likelihood(parameters[indices]) for indices, model in zip(self.param_indices, self.models)])
+        return likelihood
+
+    def prior_constraint(self, parameters: np.ndarray) -> float:
+        """
+        Apply a composite model constraint. Because of how the models
+        are implemented, the submodels can have their own independent
+        prior constraints, and this is applied prior to those.
+
+        Parameters
+        ----------
+        parameters : np.ndarray
+            NumPy 1D array containing parameters
+        """
+        return 0.
+
+    def sample_prior(self) -> np.ndarray:
+        """
+        Sample from the priors of each submodel, and ensure that the
+        number of parameters that come up match what is actually
+        expected.
+
+        Returns
+        -------
+        np.ndarray
+            NumPy 1D array with parameter values
+        """
+        parameters = np.zeros(len(self))
+        # loop over each model, sample from their prior, and organize those
+        # parameters according to the `param_indices` dictate
+        for index, model in enumerate(self.models):
+            param_indices = self.param_indices[index]
+            subparams = model.sample_prior()
+            # loop over each subparameter, and allocate to the correct parameter element
+            for param_index, subparam in zip(param_indices, subparams):
+                parameters[param_index] = subparam
+        return parameters
+
+
+class TMC1MethylChains(CompositeModel):
+    """
+    Single source, shared velocity components, shared linewidth,
+    different Ncol and Tex.
+    
+    [ss, vlsr1, vlsr2, vlsr3, vlsr4, NcolA_1, NcolA_2, NcolA_3, NcolA_4,
+    NcolB_1, NcolB_2, NcolB_3, NcolB_4, TexA, TexB, dv
+    ]
+    
+    16 parameters :P
+    
+    The idea here would be to just have the composite model do the
+    managing, and just pass the correct parameters to the correct
+    model; everything else behaves the same as they would otherwise.
+    All this child model of `CompositeModel` does is prescribe the
+    parameter indices.
+    
+    `A_state` and `E_state` are expected to be instances of `Cospatial`
+    models.
+    """
+    def __init__(self, A_state, E_state, **kwargs) -> None:
+        param_indices = [
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 13, 15],
+            [0, 1, 2, 3, 4, 9, 10, 11, 12, 14, 15]
+        ]
+        super().__init__(param_indices, A_state, E_state, **kwargs)
+
