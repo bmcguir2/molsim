9cb48b8839cb23ebad40de62824c0d88fb49eb8f
cixue <cixue.tsu@gmail.com>
2022-11-14 01:54:43 -0800

    Implement classes for managing collision data

--- molsim/radex/classes.py
+++ molsim/radex/classes.py
@@ -1,13 +1,17 @@
-from dataclasses import dataclass, field
-from typing import List, Optional
+from __future__ import annotations
+
+from dataclasses import InitVar, dataclass, field
+from functools import lru_cache, partial
+import math
+from typing import Any, Callable, Dict, FrozenSet, List, Optional, TextIO, Tuple, Type, Union
 
 import numpy as np
+from scipy.interpolate import interp1d
 
 from ..classes import Continuum, Observation, Spectrum
 from ..utils import _apply_beam, _apply_aperture
 
 from ..constants import cm, ckm, h, k
-from .interface import run as radex_run, read as radex_read
 
 
 def _planck(T, freq):
@@ -43,6 +47,254 @@ def _intersect_intervals(a, b):
         else:
             j += 1
     return intersected
+
+
+@dataclass(init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=True)
+class Levels:
+    num_levels: int
+    level_numbers: np.ndarray[int]
+    level_energies: np.ndarray[float]
+    statistical_weight: np.ndarray[float]
+    quantum_numbers: np.ndarray[str]
+    level_number_index_map: Dict[int, int]
+
+    def __repr__(self) -> str:
+        return f'<Levels object with {len(self.level_numbers)} levels>'
+
+    @classmethod
+    def from_LAMDA(cls: Type[Levels], file_in: TextIO) -> Levels:
+        fields: Dict[str, Any] = dict()
+
+        _ = file_in.readline()
+        fields['num_levels'] = int(file_in.readline().split(maxsplit=1)[0])
+
+        _ = file_in.readline()
+        fields['level_numbers'] = list()
+        fields['level_energies'] = list()
+        fields['statistical_weight'] = list()
+        fields['quantum_numbers'] = list()
+        for _ in range(fields['num_levels']):
+            cords = file_in.readline().split(maxsplit=4)
+            fields['level_numbers'].append(int(cords[0]))
+            fields['level_energies'].append(float(cords[1]))
+            fields['statistical_weight'].append(float(cords[2]))
+            fields['quantum_numbers'].append(cords[3])
+        fields['level_numbers'] = np.array(fields['level_numbers'])
+        fields['level_energies'] = np.array(fields['level_energies'])
+        fields['statistical_weight'] = np.array(fields['statistical_weight'])
+        fields['quantum_numbers'] = np.array(fields['quantum_numbers'])
+
+        fields['level_number_index_map'] = dict()
+        for index, level_number in enumerate(fields['level_numbers']):
+            fields['level_number_index_map'][level_number] = index
+
+        return cls(**fields)
+
+
+@dataclass(init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=True)
+class RadiativeTransitions:
+    num_transitions: int
+    transition_numbers: np.ndarray[int]
+    upper_level_numbers: np.ndarray[int]
+    lower_level_numbers: np.ndarray[int]
+    spontaneous_decay_rates: np.ndarray[float]
+    upper_level_indices: np.ndarray[int]
+    lower_level_indices: np.ndarray[int]
+
+    def __repr__(self:RadiativeTransitions) -> str:
+        return f'<RadiativeTransitions object with {len(self.transition_numbers)} transitions>'
+
+    @classmethod
+    def from_LAMDA(cls: Type[RadiativeTransitions], file_in: TextIO, levels: Levels) -> RadiativeTransitions:
+        fields: Dict[str, Any] = dict()
+
+        _ = file_in.readline()
+        fields['num_transitions'] = int(file_in.readline().split(maxsplit=1)[0])
+
+        _ = file_in.readline()
+        fields['transition_numbers'] = list()
+        fields['upper_level_numbers'] = list()
+        fields['lower_level_numbers'] = list()
+        fields['spontaneous_decay_rates'] = list()
+        for i in range(fields['num_transitions']):
+            cords = file_in.readline().split(maxsplit=4)
+            fields['transition_numbers'].append(int(cords[0]))
+            fields['upper_level_numbers'].append(int(cords[1]))
+            fields['lower_level_numbers'].append(int(cords[2]))
+            fields['spontaneous_decay_rates'].append(float(cords[3]))
+        fields['transition_numbers'] = np.array(fields['transition_numbers'])
+        fields['upper_level_numbers'] = np.array(fields['upper_level_numbers'])
+        fields['lower_level_numbers'] = np.array(fields['lower_level_numbers'])
+        fields['spontaneous_decay_rates'] = np.array(fields['spontaneous_decay_rates'])
+
+        def level_number_index_map_func(
+            level_number): return levels.level_number_index_map[level_number]
+        fields['upper_level_indices'] = np.array([
+            *map(level_number_index_map_func, fields['upper_level_numbers'])])
+        fields['lower_level_indices'] = np.array([
+            *map(level_number_index_map_func, fields['lower_level_numbers'])])
+
+        return cls(**fields)
+
+
+@dataclass(init=True, repr=False, eq=False, order=False, unsafe_hash=False, frozen=True)
+class CollisionalTransitions:
+    num_temperatures: int
+    num_transitions: int
+    temperatures: np.ndarray[float]
+    transition_numbers: np.ndarray[int]
+    upper_level_numbers: np.ndarray[int]
+    lower_level_numbers: np.ndarray[int]
+    rate_coefficients: np.ndarray[float]
+    upper_level_indices: np.ndarray[int]
+    lower_level_indices: np.ndarray[int]
+
+    def __repr__(self) -> str:
+        return f'<CollisionalTransitions object with {len(self.transition_numbers)} transitions at {len(self.temperatures)} temperatures>'
+
+    @classmethod
+    def from_LAMDA(cls: Type[CollisionalTransitions], file_in: TextIO, levels: Levels) -> CollisionalTransitions:
+        fields: Dict[str, Any] = dict()
+
+        _ = file_in.readline()
+        fields['num_transitions'] = int(file_in.readline().split(maxsplit=1)[0])
+        _ = file_in.readline()
+        fields['num_temperatures'] = int(file_in.readline().split(maxsplit=1)[0])
+
+        _ = file_in.readline()
+        fields['temperatures'] = [*map(float, file_in.readline().split())]
+
+        _ = file_in.readline()
+        fields['transition_numbers'] = list()
+        fields['upper_level_numbers'] = list()
+        fields['lower_level_numbers'] = list()
+        fields['rate_coefficients'] = list()
+        for i in range(fields['num_transitions']):
+            cords = file_in.readline().split()
+            fields['transition_numbers'].append(int(cords[0]))
+            fields['upper_level_numbers'].append(int(cords[1]))
+            fields['lower_level_numbers'].append(int(cords[2]))
+            fields['rate_coefficients'].append([*map(float, cords[3:])])
+        fields['transition_numbers'] = np.array(fields['transition_numbers'])
+        fields['upper_level_numbers'] = np.array(fields['upper_level_numbers'])
+        fields['lower_level_numbers'] = np.array(fields['lower_level_numbers'])
+        fields['rate_coefficients'] = np.array(fields['rate_coefficients'])
+
+        def level_number_index_map_func(level_number):
+            return levels.level_number_index_map[level_number]
+        fields['upper_level_indices'] = np.array([
+            *map(level_number_index_map_func, fields['upper_level_numbers'])])
+        fields['lower_level_indices'] = np.array([
+            *map(level_number_index_map_func, fields['lower_level_numbers'])])
+
+        return cls(**fields)
+
+
+@dataclass(init=True, repr=True, eq=False, order=False, unsafe_hash=False, frozen=True)
+class NonLTEMolecule:
+    name: str
+    weight: float
+    levels: Levels
+    radiative_transitions: RadiativeTransitions
+    collisional_transitions: Dict[str, CollisionalTransitions]
+    partner_name_standardizer: Dict[str, str] = field(init=False, repr=False)
+    collisional_rate_coefficients_getter: Dict[str, Callable[[
+        np.ndarray], np.ndarray]] = field(init=False, repr=False)
+
+    interpolator: InitVar[Union[str, Callable[[np.ndarray,
+                                               np.ndarray], Callable[[float], np.ndarray]]]] = 'slinear'
+
+    def __post_init__(self, interpolator: Union[str, Callable[[np.ndarray, np.ndarray], Callable[[float], np.ndarray]]]) -> None:
+        if isinstance(interpolator, str):
+            interpolator = partial(interp1d, kind=interpolator,
+                                   copy=False, assume_sorted=True)
+        collisional_rate_coefficients_getter: Dict[str, Callable[[float], np.ndarray]] = {
+            partner_name: lru_cache(interpolator(
+                collision_data.temperatures, collision_data.rate_coefficients))
+            for partner_name, collision_data in self.collisional_transitions.items()
+        }
+        object.__setattr__(self, 'collisional_rate_coefficients_getter',
+                           collisional_rate_coefficients_getter)
+
+        partner_name_variants = {
+            'H2':  ['h2'],
+            'pH2': ['ph2', 'p-H2', 'p-h2', 'para-H2', 'para-h2'],
+            'oH2': ['oh2', 'o-H2', 'o-h2', 'ortho-H2', 'ortho-h2'],
+            'e':   ['e-', 'electron', 'electrons'],
+            'H':   ['h'],
+            'He':  ['he'],
+            'H+':  ['h+']
+        }
+        partner_name_standardizer: Dict[str, str] = {
+            variant_name: standard_name
+            for standard_name, variant_names in partner_name_variants.items()
+            for variant_name in variant_names
+        }
+        object.__setattr__(self, 'partner_name_standardizer',
+                           partner_name_standardizer)
+
+    @classmethod
+    def from_LAMDA(cls: Type[NonLTEMolecule], file_in: Union[str, TextIO], **kwargs) -> NonLTEMolecule:
+        fields: Dict[str, Any] = dict(kwargs)
+
+        if isinstance(file_in, str):
+            file_in = open(file_in, 'r')
+
+        """
+        link: https://home.strw.leidenuniv.nl/~moldata/molformat.html
+
+        Format of LAMDA datafiles
+        Below follows a description of the format adopted for presenting the atomic and molecular data in LAMDA. Any similarities with datafiles from other authors is completely coincidental.
+
+        Lines 1 - 2: molecule (or atom) name
+        Lines 3 - 4: molecular (or atomic) weight (a.m.u.)
+        Lines 5 - 6: number of energy levels (NLEV)
+        Lines 7 - 7+NLEV: level number, level energy (cm-1), statistical weight. These numbers may be followed by additional info such as the quantum numbers, which are however not used by the program. The levels must be listed in order of increasing energy.
+        Lines 8+NLEV - 9+NLEV: number of radiative transitions (NLIN)
+        Lines 10+NLEV - 10+NLEV+NLIN: transition number, upper level, lower level, spontaneous decay rate (s-1). These numbers may be followed by additional info such as the line frequency, which is however not used by the program.
+        Lines 11+NLEV+NLIN - 12+NLEV+NLIN: number of collision partners (NPART)
+        This is followed by NPART blocks of collision data:
+        Lines 13+NLEV+NLIN - 14+NLEV+NLIN: collision partner ID and reference. Valid identifications are: 1=H2, 2=para-H2, 3=ortho-H2, 4=electrons, 5=H, 6=He, 7=H+.
+        Lines 15+NLEV+NLIN - 16+NLEV+NLIN: number of transitions for which collisional data exist (NCOL)
+        Lines 17+NLEV+NLIN - 18+NLEV+NLIN: number of temperatures for which collisional data are given
+        Lines 19+NLEV+NLIN - 20+NLEV+NLIN: the NTEMP values of the temperature for which collisional data are given
+        Lines 21+NLEV+NLIN - 21+NLEV+NLIN+NCOL: transition number, upper level, lower level; rate coefficients (cm3s-1) at each temperature.
+        """
+        _ = file_in.readline()
+        fields['name'] = file_in.readline().rstrip()
+
+        _ = file_in.readline()
+        fields['weight'] = float(file_in.readline().split(maxsplit=1)[0])
+
+        fields['levels'] = Levels.from_LAMDA(file_in)
+
+        fields['radiative_transitions'] = RadiativeTransitions.from_LAMDA(
+            file_in, fields['levels'])
+
+        LAMDA_partner_id_name_map = {
+            1: 'H2',
+            2: 'pH2',
+            3: 'oH2',
+            4: 'e',
+            5: 'H',
+            6: 'He',
+            7: 'H+'
+        }
+
+        _ = file_in.readline()
+        num_partners = int(file_in.readline().split(maxsplit=1)[0])
+        fields['collisional_transitions'] = dict()
+        for i in range(num_partners):
+            _ = file_in.readline()
+            partner_id = int(file_in.readline().split(maxsplit=1)[0])
+            if partner_id not in LAMDA_partner_id_name_map:
+                raise ValueError(f'Unregconized collision partner ID: {partner_id = }')
+
+            partner_name = LAMDA_partner_id_name_map[partner_id]
+            fields['collisional_transitions'][partner_name] = CollisionalTransitions.from_LAMDA(
+                file_in, fields['levels'])
+
+        return cls(**fields)
 
 
 @dataclass
